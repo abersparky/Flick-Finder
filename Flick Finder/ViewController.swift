6030abf068db5ba47f8f7862e1b98c5b7351d043
Christopher <abersparky@gmail.com>
2015-05-29 17:59:26 -0700

    Tap & Return keyboard dismiss, search field hookup

--- Flick Finder/ViewController.swift
+++ Flick Finder/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
      @IBOutlet weak var flickImage: UIImageView!
      @IBOutlet weak var searchTextField: UITextField!
@@ -20,26 +20,146 @@ class ViewController: UIViewController {
      
      let BASE_URL = "https://api.flickr.com/services/rest/"
      
+     var tapRecognizer: UITapGestureRecognizer? = nil
+     
+     
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          // Do any additional setup after loading the view, typically from a nib.
+          
+          searchTextField.delegate = self
+          latTextField.delegate = self
+          longTextField.delegate = self
+          
+          tapRecognizer = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
+          tapRecognizer?.numberOfTapsRequired = 1
+     }
+
+     override func didReceiveMemoryWarning() {
+          super.didReceiveMemoryWarning()
+          // Dispose of any resources that can be recreated.
+     }
+     
+     
+     override func viewWillAppear(animated: Bool) {
+          super.viewWillAppear(animated)
+          
+          println("Add the tapRecognizer and subscribe to keyboard notifications in viewWillAppear")
+          
+          /* Add tap recognizer to dismiss keyboard */
+          self.addKeyboardDismissRecognizer()
+          
+          self.subscribeToKeyboardNotifications()
+     }
+     
+     override func viewWillDisappear(animated: Bool) {
+          super.viewWillDisappear(animated)
+          
+          println("Remove the tapRecognizer and unsubscribe from keyboard notifications in viewWillDisappear")
+          
+          /* Remove tap recognizer */
+          self.removeKeyboardDismissRecognizer()
+          
+          self.unsubscribeToKeyboardNotifications()
+     }
+     
+     
+     
+     
+     /* ============================================================
+     * Functional stubs for handling UI problems
+     * ============================================================ */
+     
+     /* 1 - Dismissing the keyboard */
+     func addKeyboardDismissRecognizer() {
+          println("Add the recognizer to dismiss the keyboard")
+          self.view.addGestureRecognizer(tapRecognizer!)
+     }
+     
+     func removeKeyboardDismissRecognizer() {
+          println("Remove the recognizer to dismiss the keyboard")
+          self.view.removeGestureRecognizer(tapRecognizer!)
+     }
+     
+     func handleSingleTap(recognizer: UITapGestureRecognizer) {
+          println("End editing here")
+          self.view.endEditing(true)
+     }
+     
+     /* 2 - Shifting the keyboard so it does not hide controls */
+     func subscribeToKeyboardNotifications() {
+          println("Subscribe to the KeyboardWillShow and KeyboardWillHide notifications")
+          NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+          NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+     }
+     
+     func unsubscribeToKeyboardNotifications() {
+          println("Unsubscribe to the KeyboardWillShow and KeyboardWillHide notifications")
+          NSNotificationCenter.defaultCenter().removeObserver(self, name:
+               UIKeyboardWillShowNotification, object: nil)
+          NSNotificationCenter.defaultCenter().removeObserver(self, name:
+               UIKeyboardWillHideNotification, object: nil)
+     }
+     
+     func keyboardWillShow(notification: NSNotification) {
+          println("Shift the view's frame up so that controls are shown")
+          if searchTextField.isFirstResponder() || longTextField.isFirstResponder() || latTextField.isFirstResponder() {
+               self.view.frame.origin.y -= getKeyboardHeight(notification)
+          }
+     }
+     
+     func keyboardWillHide(notification: NSNotification) {
+          println("Shift the view's frame down so that the view is back to its original placement")
+          if searchTextField.isFirstResponder() || longTextField.isFirstResponder() || latTextField.isFirstResponder() {
+               self.view.frame.origin.y = 0
+          }
+     }
+     
+     func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+          println("Get and return the keyboard's height from the notification")
+          let userInfo = notification.userInfo
+          let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+          return keyboardSize.CGRectValue().height
+     }
+     
+     // Dismisses keyboard when return is pressed
+     func textFieldShouldReturn(textField: UITextField) -> Bool {
+          searchTextField.resignFirstResponder()
+          longTextField.resignFirstResponder()
+          latTextField.resignFirstResponder()
+          return true
+     }
+     
+     
+     
+     
+     
      
      
      @IBAction func searchTextButtonTouchUp(sender: AnyObject) {
           
-
+          /* Added from student request -- hides keyboard after searching */
+          self.dismissAnyVisibleKeyboards()
           
           
-          let keyValuePairs = [
-               "method": "flickr.photos.search",
-               "api_key": "f23a3195f2fb63a93781aeb1421f26b4",
-               "text": "baby+asian+elephant",
-               "safe_search": "1",
-               "extras": "url_m",
-               "format": "json",
-               "nojsoncallback": "1"
-          ]
+          if (searchTextField.text == "") {
+               placeholderLabel.text = "Please enter a search value below."
+          }
+          else {
+               placeholderLabel.alpha = 0
           
-          getImageFromFlickrBySearch(keyValuePairs)
+               let keyValuePairs = [
+                    "method": "flickr.photos.search",
+                    "api_key": "f23a3195f2fb63a93781aeb1421f26b4",
+                    "text": searchTextField.text,
+                    "safe_search": "1",
+                    "extras": "url_m",
+                    "format": "json",
+                    "nojsoncallback": "1"
+               ]
           
-          
+               getImageFromFlickrBySearch(keyValuePairs)
+          }
      }
      
      @IBAction func searchLatLongButtonTouchUp(sender: AnyObject) {
@@ -47,15 +167,7 @@ class ViewController: UIViewController {
      }
      
      
-     override func viewDidLoad() {
-          super.viewDidLoad()
-          // Do any additional setup after loading the view, typically from a nib.
-     }
-
-     override func didReceiveMemoryWarning() {
-          super.didReceiveMemoryWarning()
-          // Dispose of any resources that can be recreated.
-     }
+   
 
      func getImageFromFlickrBySearch(methodArguments: [String : AnyObject]) {
           
@@ -74,7 +186,52 @@ class ViewController: UIViewController {
                } else {
                     var parsingError: NSError? = nil
                     let parsedResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
-                    println(parsedResult.valueForKey("photos"))
+                    
+                    // store the parsed photos into a dictionary
+                    if let photosDictionary = parsedResult.valueForKey("photos") as? NSDictionary {
+                         
+                         if let photoArray = photosDictionary.valueForKey("photo") as? [[String: AnyObject]] {
+                              
+                              // grab a random image index
+                              let randomPhotoIndex = Int(arc4random_uniform(UInt32(photoArray.count)))
+                              
+                              let photoDictionary = photoArray[randomPhotoIndex] as [String: AnyObject]
+                              
+                              // get the url and title from dictionary
+                                   
+                              let photoTitle = photoDictionary["title"] as? String
+                              let imageURLstring = photoDictionary["url_m"] as? String
+                              let imageURL = NSURL(string: imageURLstring!)
+                              
+                              // if image exists, set the image and title
+                              
+                              
+                              if let imageData = NSData(contentsOfURL: imageURL!) {
+                                   dispatch_async(dispatch_get_main_queue(), {
+                                        self.flickImage.image = UIImage(data: imageData)
+                                        self.imageTitle.text = photoTitle
+                                   })
+                              } else {
+                                   println("Image does not exist at \(imageURL)")
+                              }
+                              
+                              
+                              
+                         } else {
+                              println("Cannot find key 'photo' in \(photosDictionary)")
+                         }
+                         
+                         
+                         
+                         
+                         
+                    } else {
+                         println("Can't find key 'photos' in \(parsedResult)")
+                    }
+                    
+
+                    
+   
                }
           }
           
@@ -103,4 +260,14 @@ class ViewController: UIViewController {
           return (!urlVars.isEmpty ? "?" : "") + join("&", urlVars)
      }
 }
+     
+     /* This extension was added as a fix based on student comments */
+     extension ViewController {
+          func dismissAnyVisibleKeyboards() {
+               if searchTextField.isFirstResponder() || latTextField.isFirstResponder() || longTextField.isFirstResponder() {
+                    self.view.endEditing(true)
+               }
+          }
+     }
+
 
